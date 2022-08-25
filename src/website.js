78d8b0263b8ea821abcebfdac5843acc53f14e13
MichalTrostowiecki <michal.trostowiecki@gmail.com>
2022-08-25 20:27:00 +0100

    I'm currently working on option to add things to do to main display

--- src/website.js
+++ src/website.js
@@ -6,18 +6,33 @@ function loadPage() {
 
   displayInbox(mainDiv);
   displayProjects(mainDiv);
-  addNewThing(mainDiv);
+  addButton(mainDiv);
 }
 
-function addNewThing(mainDiv) {
+function addButton(mainDiv) {
   const addButton = document.createElement("button");
   addButton.textContent = "+";
   addButton.classList.add("addButton");
-  addButton.addEventListener("click", () => {
-    console.log("clicked");
-  });
-
   mainDiv.appendChild(addButton);
+
+  addButton.addEventListener("click", newThing(mainDiv));
 }
+
+function newThing(mainDiv) {
+  const inputDiv = document.createElement("div");
+  inputDiv.classList.add("inputDiv");
+
+  const input = document.createElement("input");
+  input.classList.add("tittle");
+
+  mainDiv.appendChild(inputDiv);
+  mainDiv.appendChild(input);
+
+  const userInput = document.createElement("p");
+  userInput.textContent = input.value;
+  mainDiv.appendChild(userInput);
+}
+
+function addForm() {}
 
 export default loadPage;
